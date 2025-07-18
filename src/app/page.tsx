4c58822ce7e3d651aa2537b2edda962946c5de38
taha0998 <taha2020tahataha@gmail.com>
2025-07-18 23:41:43 +0100

    changes in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,13 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
-            .
+            MAMAlia
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+         
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
