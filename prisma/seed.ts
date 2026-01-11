d1876f11668177a9c0ccbad973fe509497a5737d
taha0998 <taha2020tahataha@gmail.com>
2026-01-11 10:07:37 +0100

    prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+
+
+const tickets = [
+    {
+        title: 'Ticket 1',
+        content: 'this is the first ticket from database',
+        status: 'DONE' as const,
+    },
+    {
+        title: 'Ticket 2',
+        content: 'this is the second ticket from database',
+        status: 'OPEN' as const,
+    }, {
+        title: 'Ticket 3',
+        content: 'this is the third ticket from database',
+        status: 'IN_PROGRESS' as const,
+    }
+]
+const seed = async () => {
+    const t0 = performance.now();
+    console.log("DB Seed: Started...")
+    await prisma.ticket.deleteMany();
+
+    await prisma.ticket.createMany({
+        data: tickets,
+    })
+    const t1 = performance.now();
+    console.log(`DB Seed: Finished (${(t1 - t0).toFixed()}ms)`)
+}
+
+seed()
